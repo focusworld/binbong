ecd46760efdf7461c46577020190929bede247e0
focusworld <rezaabsy@yahoo.com>
2016-02-21 21:45:15 +0330

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,29 @@
+do
+
+function run(msg, matches)
+  return 'نرخ فروش گروه های فوکوس'.. [[ 
+  
+  
+گروه با تمام امکانات 
+1 ماه 2 تومن
+2 ماه 3 تومن
+6 ماه 5 تومن
+نامحدود 10 تومن
+
+  
+برای خرید به ای دی زیر پیام دهید
+@mamaligod
+  Engineer mamali godem]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/nerkh : nerkhbot",
+  patterns = {
+    "^[!/]nerkh$",
+    "^nerkh$"
+  }, 
+  run = run 
+}
+
+end
