79381df104d33f0966f4164fc148367ec12c8f25
focusworld <rezaabsy@yahoo.com>
2016-02-21 21:46:24 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,28 @@
+do
+
+function run(msg, matches)
+  return 'focus Telegram Bot v1.5'.. [[ 
+  
+  
+  Antispam Bot: @focus_asp
+  Channel: @focus_team
+  Sudo1 (Admin): @mamaligod
+  Sudo2 (Admin): @mamaligodem
+  Sudo3 (Admin): @xxx_sargardan_xxx
+  
+  Powered by:
+  focus team Co. Group
+  Engineer mamali godem]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$",
+    "^ver$"
+  }, 
+  run = run 
+}
+
+end
